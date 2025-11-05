20ccd243f78ef9d18a3d734408158bcdbeeb3a7c
juzaetum <jzaetum@gmail.com>
2025-11-04 22:01:33 -0300

    AC2 Application Test 3

--- src/test/java/ac2_project/example/ac2_ca/Ac2CaApplicationTests.java
+++ src/test/java/ac2_project/example/ac2_ca/Ac2CaApplicationTests.java
@@ -2,8 +2,11 @@ package ac2_project.example.ac2_ca;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
 
 class Ac2CaApplicationTest {
 
@@ -16,10 +19,13 @@ class Ac2CaApplicationTest {
 	}
 
 	@Test
-	@DisplayName("Deve executar o método main sem erros")
-	void deveExecutarMainSemErros() {
-		// Garante que o método main roda sem lançar exceções
-		Ac2CaApplication.main(new String[] {});
-		assertThat(true).isTrue(); // se chegou até aqui, está tudo certo
+	@DisplayName("Deve chamar SpringApplication.run() no método main() sem realmente subir contexto")
+	void deveExecutarMainSemSubirContexto() {
+		try (MockedStatic<SpringApplication> mocked = mockStatic(SpringApplication.class)) {
+			// executa o método main e verifica se SpringApplication.run foi chamado
+			Ac2CaApplication.main(new String[] {});
+			mocked.verify(() -> SpringApplication.run(Ac2CaApplication.class, new String[] {}));
+		}
+		assertThat(true).isTrue(); // só pra manter o assert final
 	}
 }
